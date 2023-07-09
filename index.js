0701be08363abbd6d5a2a3dfbf83423c72d383d4
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-09 15:41:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
+console.log(isPalindrome("Mom"));
+console.log(isPalindrome("Taco cat"));
+console.log(isPalindrome("Sit on a potato pan Otis"));
 /* 
   Add your pseudocode here
+//convert word string to lowercas and remove spaces and store in words variable
+//reverse the string
+//check if the original string and reversed string are equal
+//return true if they are equalor false otherwise
 */
 
 /*
   Add written explanation of your solution here
+  //convert the string to lowercase and remove spaces
+    word = lowercase(str)
+    word = removeSpaces(str)
+  //reverse the string
+    reversedStr = reverseString(word)
+   //check if the original string and reversed string are equal
+    if word equals reversedword:
+        return true
+    else:
+        return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +41,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
